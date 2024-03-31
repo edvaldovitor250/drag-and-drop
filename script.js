d514c75f57ed784e8ba7a8ccf14a3860e2361413
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-30 21:18:54 -0300

    add: logic of game

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.querySelectorAll('.item').forEach(item =>{
     item.addEventListener('dragstart', dragStart)
     item.addEventListener('dragend', dragEnd)
-
 })
 
-// Funcitions Item
+document.querySelectorAll('.area').forEach(area =>{
+    area.addEventListener('dragover', dragOver)
+    area.addEventListener('dragleave', dragLeave)
+    area.addEventListener('drop', drop)
+})
+
+// Funções Item
 function dragStart(e) {
     e.currentTarget.classList.add('dragging')
 }
@@ -13,4 +18,23 @@ function dragEnd(e) {
     e.currentTarget.classList.remove('dragging')
 }
 
-// Functions Area
+// Funções Área
+function dragOver(e) {
+    e.preventDefault(); 
+    if (!e.currentTarget.classList.contains('item')) {
+        e.currentTarget.classList.add('hover')
+    }
+}
+
+function dragLeave(e) {
+    e.currentTarget.classList.remove('hover')
+}
+
+function drop(e) {
+    e.currentTarget.classList.remove('hover')
+    let dragItem = document.querySelector('.item.dragging')
+
+    if (!e.currentTarget.classList.contains('item')) {
+        e.currentTarget.appendChild(dragItem)
+    }
+}
