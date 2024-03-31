8c3b69fbdecc13019a2efab16ccbf51e28d3340f
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-30 21:02:07 -0300

    add: function  dragStart and dragEnd

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ document.querySelectorAll('.item').forEach(item =>{
     item.addEventListener('dragend', dragEnd)
 
 })
+
+// Funcitions Item
+function dragStart(e) {
+    e.currentTarget.classList.add('dragging')
+}
+
+function dragEnd(e) {
+    e.currentTarget.classList.remove('dragging')
+}
+
+// Functions Area
