bf840f34c26cc39e5f519cedf09ba2f81270cd0c
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-30 21:23:57 -0300

    add: function neutral

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ document.querySelectorAll('.area').forEach(area =>{
     area.addEventListener('drop', drop)
 })
 
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral)
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral)
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral)
+
 // Funções Item
 function dragStart(e) {
     e.currentTarget.classList.add('dragging')
@@ -37,4 +41,23 @@ function drop(e) {
     if (!e.currentTarget.classList.contains('item')) {
         e.currentTarget.appendChild(dragItem)
     }
+}
+
+
+// Functions Neutral Area
+
+function dragOverNeutral(e) {
+    e.preventDefault(); 
+        e.currentTarget.classList.add('hover')
+    
+}
+
+function dragLeaveNeutral(e) {
+    e.currentTarget.classList.remove('hover')
+}
+
+function dropNeutral(e) {
+    e.currentTarget.classList.remove('hover')
+    let dragItem = document.querySelector('.item.dragging')
+    e.currentTarget.appendChild(dragItem)
 }
